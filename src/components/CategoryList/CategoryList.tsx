18468b546a657205719f606b5f68cece1c67ac56
dudelis <fukszon.kostya@gmail.com>
2024-04-08 11:04:07 +0200

    adds Category List component - initial draft

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,13 +1,54 @@
 import * as React from 'react';
 import styles from './categorylist.module.css'
+import Link from 'next/link';
+import Image from 'next/image';
 
-export interface ICategoryListProps {
+export type TCategory = {
+  name: string,
+  color: string,
+  imageUrl: string
+}
+export type TCategoryListProps = {
+  categories: TCategory[]
 }
 
-export default function CategoryList (props: ICategoryListProps) {
+const categoryListProps: TCategoryListProps = {
+  categories: [
+    {
+      name: "Power Platform",
+      color: "pink",
+      imageUrl: "/style.png"
+    },
+    {
+      name: "Next JS",
+      color: "grey",
+      imageUrl: "/fashion.png"
+    },
+    {
+      name: "DevOps",
+      color: "blue",
+      imageUrl: "/food.png"
+    }
+  ]
+}
+
+
+export default function CategoryList(props: TCategoryListProps) {
   return (
     <div className={styles.container}>
-      CategoryList
+      <h2 className={styles.title}>Popular Categories</h2>
+      <div className={styles.categories}>
+        {categoryListProps.categories.map((item, index) => (
+          <Link key={index} href={`/blog?cat=${item.name.toLowerCase()}`}
+            className={`${styles.category} ${styles[item.name.toLocaleLowerCase()]}`}
+            style={{background: item.color}}
+            >
+            <Image src={item.imageUrl} alt="" width={32} height={32} className={styles.image} />
+            {item.name}
+          </Link>
+        ))}
+
+      </div>
     </div>
   );
 }
