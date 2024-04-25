8e273402cf6cc388383e644f4621b2fcf1a9bea4
dudelis <fukszon.kostya@gmail.com>
2024-04-25 18:59:39 +0200

    replaces CSS in Navbar component with tailwind - #10

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,24 +2,26 @@ import * as React from 'react';
 import styles from './footer.module.css'
 import Image from 'next/image';
 import Link from 'next/link';
+import { LogoIcons } from '../logo-icons/logoicons';
 
 export interface IFooterProps {
 }
 
 export default function Footer(props: IFooterProps) {
   return (
-    <footer className={styles.container}>
-      <div className={styles.info}>
-        <div className={styles.logoConainer}>
+    <footer className="flex justify-between text-primary-foreground py-8 items-center gap-4">
+      <div className="flex flex-[1] flex-col gap-4">
+        <div className="flex items-center gap-4">
           <Image src="/codemusicianlogo.png" alt="logo" width={100} height={100} />
           <div className={styles.logoText}>Code Musician Blog</div>
         </div>
         <div className={styles.footerContent}>
-          <p className={styles.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tempora sapiente maiores inventore laboriosam culpa, suscipit necessitatibus culpa, magni amet deleniti ea laborum!</p>
+          <p className={styles.description}>
+            Welcome to Code Musician Blog, where ones and zeros dance with melodies, and binary harmonizes with beats!
+            Strum along with me as we explore the intersection of music and tech. From “How to Tune Your Guitar (and Your Wi-Fi)” to “APIs in A Minor,” we’ll hit all the right notes. And if you’ve ever wondered what a software engineer’s favorite chord is, it’s definitely “C# Major” (because it’s sharp).
+          </p>
           <div className={styles.icons}>
-            <Image src="/facebook.png" alt="facebook logo" width={24} height={24} />
-            <Image src="/youtube.png" alt="youtube logo" width={24} height={24} />
-            <Image src="/linkedin.png" alt="linkedin logo" width={24} height={24} />
+            <LogoIcons />
           </div>
         </div>
       </div>
