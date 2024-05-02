c2d25ab72a1f1d071f1d23d283ef04f4f65d8d4c
dudelis <fukszon.kostya@gmail.com>
2024-05-02 12:14:00 +0200

    #10 removed css from Footer component

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -19,16 +19,16 @@ export interface IFeaturedProps {
 export default async function Featured(props: IFeaturedProps) {
   const post = getFeaturedPost();
   return (
-    <section className="mt-8 md:mt-12 flex gap-8 justify-between items-center">
+    <section className="flex items-center justify-between gap-8 mt-8 md:mt-12">
       <div className="hidden lg:flex relative h-96 flex-[1]">
         {post && <Image src={post.image} alt="" fill className="object-cover" />}
       </div>
       <div className="flex-[1] flex flex-col gap-4 text-foreground">
         <h2 className="text-xl md:text-2xl lg:text-3xl">{post?.title}</h2>
-        <div className="text-sm md:text-base font-light prose text-foreground prose-headings:text-foreground">
+        <div className="text-sm font-light prose md:text-base text-foreground prose-headings:text-foreground">
           {post && <MDXRemote source={`${post.content.substring(0, 300)}...`}/>}
         </div>
-        <Link href={`/posts/${post?.slug}`} className="py-4 px-6 w-max bg-accent text-background">Read More</Link>
+        <Link href={`/posts/${post?.slug}`} className="px-6 py-4 w-max bg-accent text-background">Read More</Link>
       </div>
     </section>
   );
