70f2ec1b2a28c3afa961448e09829664d7c20c31
dudelis <fukszon.kostya@gmail.com>
2024-05-08 13:15:10 +0200

    #10 removed css from posts/page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import styles from './singlePage.module.css';
 import { Spacer } from '@/components/Spacer/Spacer';
 import Image, { ImageProps } from 'next/image';
 import Menu from '@/components/Menu/Menu';
@@ -63,40 +62,42 @@ export async function generateStaticParams() {
 export default async function SinglePage(props: TSinglePostProps) {
   const post = getSinglePost(props.params.slug as string);
   return (
-    <div className={styles.container}>
+    <div className="flex gap-4 2xl:gap-8 flex-col">
       <Spacer />
-      <div className={styles.infoContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>{post.title}</h1>
-          <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src="/me-captain.jpeg" alt="" width={300} height={300} className={styles.userImage} />
+      <section className="flex gap-8 items-center flex-col md:flex-row">
+        <div className="flex flex-col flex-[1] justify-between gap-4 2xl:gap-12">
+          <h1 className="text-3xl lg:text-4xl xl:text-5xl flex">{post.title}</h1>
+          <div className="flex flex-[1] gap-4 items-center">
+            <div className="relative 
+              h-12 w-12
+              2xl:h-16 2xl:w-16">
+              <Image src="/me-captain.jpeg" alt="" width={300} height={300} className="rounded-full object-cover" />
             </div>
-            <div className={styles.userTextContainer}>
-              <span className={styles.userName}>Konstantin Fukszon</span>
-              <span className={styles.postDate}>{formatDate(post.date)}</span>
+            <div className="flex gap-[0.1] flex-col text-muted-foreground text-sm">
+              <span>Konstantin Fukszon</span>
+              <span>{formatDate(post.date)}</span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <Image src={post.image} alt="" layout="fill" className={styles.image} />
+        <div className="flex relative h-70 xl:h-90 2xl:h-120 flex-[1]">
+          <Image src={post.image} alt="" width={300} height={300} className="object-cover rounded w-full h-[calc(100%-1rem)]" />
         </div>
-      </div>
+      </section>
       <Spacer />
-      <div className={styles.content}>
-        <div className={styles.post}>
+      <section className="flex gap-12">
+        <div className="flex-[3]">
           <article className="font-normal prose text-md prose-no-quotes prose-blockquote:text-accent text-foreground prose-headings:mt-8 prose-headings:font-semibold prose-headings:text-foreground prose-h1:text-5xl prose-h2:text-4xl prose-h3:text-3xl prose-h4:text-2xl prose-h5:text-xl prose-h6:text-lg prose-a:text-accent" >
             <MDXRemote source={post.content} />
           </article>
           <Spacer />
           <Comments postslug={post.slug} />
         </div>
-        <div className={styles.rightMenu}>
-          <Menu />
+        <div className="flex-1">
+          {/* <Menu /> */}
           <Spacer />
           {/* <MenuCategories /> */}
         </div>
-      </div>
+      </section>
     </div>
   );
 }
