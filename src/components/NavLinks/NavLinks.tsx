72e6037118aaae172744a3cc0107baa4a2ad7b22
dudelis <fukszon.kostya@gmail.com>
2024-06-01 22:11:33 +0200

    fixes closing menu #20

--- src/components/NavLinks/NavLinks.tsx
+++ src/components/NavLinks/NavLinks.tsx
@@ -4,12 +4,14 @@ import Link from 'next/link';
 import AuthLinks from '../AuthLinks/AuthLinks';
 import ThemeToggle from '../ThemeToggle/ThemeToggle';
 import { cn } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
 
 export interface INavLinksProps {
 }
 
 export default function NavLinks(props: INavLinksProps) {
   const [open, setOpen] = React.useState(false);
+  const router = useRouter();
   return (
     <div className="flex gap-2 text-xs flex-[2] justify-end items-center 
       lg:gap-3 lg:text-sm 
@@ -18,7 +20,7 @@ export default function NavLinks(props: INavLinksProps) {
       <Link className="hidden sm:block" href="/">Home</Link>
       <Link className="hidden sm:block" href="/contacts">Contacts</Link>
       <Link className="hidden sm:block" href="/about">About</Link>
-      <AuthLinks />
+      <AuthLinks setOpen={setOpen} open={false} />
       <div className="relative flex flex-col justify-between w-5 h-4 bg-transparent border-none cursor-pointer sm:hidden" onClick={() => setOpen(!open)}>
         <span className={cn(
           "bg-foreground h-[0.1rem] w-full transition ease-in-out duration-300",
@@ -35,10 +37,10 @@ export default function NavLinks(props: INavLinksProps) {
         absolute top-20 left-0 bg-background w-full h-[calc(100vh-5rem)] 
         flex flex-col items-center justify-center gap-8 text-2xl font-bold z-50
         ">
-          <Link href="/">Home</Link>
-          <Link href="/contacts">Contacts</Link>
-          <Link href="/about">About</Link>
-          <AuthLinks />
+          <Link href="/" onClick={() => setOpen(false)}>Home</Link>
+          <Link href="/contacts" onClick={() => setOpen(false)}>Contacts</Link>
+          <Link href="/about" onClick={() => setOpen(false)}>About</Link>
+          <AuthLinks setOpen={setOpen} open />
         </div>
       )}
     </div>
